1c76de7d52b661e3b5ec6df3820ffa030784517d
Max Burlaka <maksbyrlaka69@gmail.com>
2021-12-17 20:32:48 +0200

    p071

---
+++ p071/src/main/java/com/burlakamax/myapplication/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.burlakamax.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val textTitle = findViewById<TextView>(R.id.textView)
+        val text = findViewById<TextView>(R.id.textView2)
+        val img = findViewById<ImageView>(R.id.imageView)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+
+        radioGroup.setOnCheckedChangeListener { radioGroup, i ->
+            when(i){
+                R.id.radioButton2 ->  textTitle.setText("rb1")
+                R.id.radioButton4 ->  textTitle.setText("rb2")
+            }
+        }
+
+        return when (item.itemId) {
+
+            R.id.electric_guitar -> {
+                textTitle.setText(getString(R.string.electric_guitar))
+                text.setText(getString(R.string.electric_guitar1))
+                img.setImageResource(R.drawable.electric_guitar)
+                true
+            }
+            R.id.acoustic_guitar -> {
+                textTitle.setText(getString(R.string.acoustic_guitar))
+                text.setText(getString(R.string.acoustic_guitar1))
+                img.setImageResource(R.drawable.acoustic_guitar)
+                true
+            }
+            R.id.bass_guitar -> {
+                textTitle.setText(getString(R.string.bass_guitar))
+                text.setText(getString(R.string.bass_guitar1))
+                img.setImageResource(R.drawable.bass_guitar)
+                true
+            }
+            R.id.small_drum -> {
+                textTitle.setText(getString(R.string.small_drum))
+                text.setText(getString(R.string.small_drum1))
+                img.setImageResource(R.drawable.small_drum)
+                true
+            }
+            R.id.big_drum -> {
+                textTitle.setText(getString(R.string.big_drum))
+                text.setText(getString(R.string.big_drum1))
+                img.setImageResource(R.drawable.big_drum)
+                true
+            }
+            R.id.system_drum -> {
+                textTitle.setText(getString(R.string.system_drum))
+                text.setText(getString(R.string.system_drum1))
+                img.setImageResource(R.drawable.system_drum)
+                true
+            }
+            R.id.acoustic_piano -> {
+                textTitle.setText(getString(R.string.acoustic_piano))
+                text.setText(getString(R.string.acoustic_piano1))
+                img.setImageResource(R.drawable.acoustic_piano)
+                true
+            }
+            R.id.accordion -> {
+                textTitle.setText(getString(R.string.accordion))
+                text.setText(getString(R.string.accordion1))
+                img.setImageResource(R.drawable.accordion)
+                true
+            }
+            R.id.synthesizers -> {
+                textTitle.setText(getString(R.string.synthesizers))
+                text.setText(getString(R.string.synthesizers1))
+                img.setImageResource(R.drawable.synthesizers)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
