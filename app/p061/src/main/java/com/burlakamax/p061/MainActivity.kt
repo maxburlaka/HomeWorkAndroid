68b13c7a02a288d1e91f363d4f5a73cfc4d074a9
Max Burlaka <maksbyrlaka69@gmail.com>
2021-12-17 16:26:57 +0200

    new_p061

--- app/p061/src/main/java/com/burlakamax/p061/MainActivity.kt
+++ app/p061/src/main/java/com/burlakamax/p061/MainActivity.kt
@@ -10,53 +10,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var text0neTeam = findViewById(R.id.textView3) as TextView
-        var textTwoTeam = findViewById(R.id.textView4) as TextView
-        var buttonOnePoints = findViewById(R.id.p1) as Button
-        var buttonTwoPoints = findViewById(R.id.p2) as Button
-        var buttonTrePoints = findViewById(R.id.p3) as Button
-        var buttonOnePoints1 = findViewById(R.id.p11) as Button
-        var buttonTwoPoints1 = findViewById(R.id.p22) as Button
-        var buttonTrePoints1 = findViewById(R.id.p33) as Button
-        var buttonReset = findViewById(R.id.reset) as Button
+        val text0neTeam = findViewById<TextView>(R.id.textView3)
+        val textTwoTeam = findViewById<TextView>(R.id.textView4)
+        val buttonOnePoints = findViewById<Button>(R.id.p1)
+        val buttonTwoPoints = findViewById<Button>(R.id.p2)
+        val buttonTrePoints = findViewById<Button>(R.id.p3)
+        val buttonOnePoints1 = findViewById<Button>(R.id.p11)
+        val buttonTwoPoints1 = findViewById<Button>(R.id.p22)
+        val buttonTrePoints1 = findViewById<Button>(R.id.p33)
+        val buttonReset = findViewById<Button>(R.id.reset)
         var s1 = 0
         var s2 = 0
 
         buttonOnePoints.setOnClickListener{
             s1 = s1 + 1
-            text0neTeam.setText("$s1")
+            text0neTeam.text = s1.toString()
         }
 
         buttonTwoPoints.setOnClickListener {
             s1 = s1 + 2
-            text0neTeam.setText("$s1")
+            text0neTeam.text = s1.toString()
         }
 
         buttonTrePoints.setOnClickListener {
             s1 = s1 + 3
-            text0neTeam.setText("$s1")
+            text0neTeam.text = s1.toString()
         }
 
         buttonOnePoints1.setOnClickListener{
             s2 = s2 + 1
-            textTwoTeam.setText("$s2")
+            textTwoTeam.text = s2.toString()
         }
 
         buttonTwoPoints1.setOnClickListener {
             s2 = s2 + 2
-            textTwoTeam.setText("$s2")
+            textTwoTeam.text = s2.toString()
         }
 
         buttonTrePoints1.setOnClickListener {
             s2 = s2 + 3
-            textTwoTeam.setText("$s2")
+            textTwoTeam.text = s2.toString()
         }
 
         buttonReset.setOnClickListener {
             s1 = 0
             s2 = 0
-            text0neTeam.setText("$s1")
-            textTwoTeam.setText("$s2")
+            text0neTeam.text = s1.toString()
+            textTwoTeam.text = s2.toString()
         }
     }
 }
